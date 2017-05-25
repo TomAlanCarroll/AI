ae298cbb7bc0d7eb21f43fcb29ce4962ca96e179
Tom Carroll <tomalancarroll@gmail.com>
2017-05-25 10:54:07 +0200

    Moved most functions from sudoku to utils

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,100 +1,11 @@
 from utils import *
 
-def grid_values(grid):
-    """Convert grid string into {<box>: <value>} dict with '123456789' value for empties.
+def search(values):
+    "Using depth-first search and propagation, create a search tree and solve the sudoku."
+    # First, reduce the puzzle using the previous function
 
-    Args:
-        grid: Sudoku grid in string form, 81 characters long
-    Returns:
-        Sudoku grid in dictionary form:
-        - keys: Box labels, e.g. 'A1'
-        - values: Value in corresponding box, e.g. '8', or '123456789' if it is empty.
-    """
-    assert len(grid) == 81, 'grid for grid_values must be 81 characters'
-    grid_dict = dict(zip(boxes, grid))
-    for key,val in grid_dict.items():
-        if val == '.':
-            grid_dict[key] = '123456789'
+    # Choose one of the unfilled squares with the fewest possibilities
 
-    return grid_dict
+    # Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
 
-# Tests:
-#display(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'))
-#display(grid_values('483921657967345821251876493548132976729564138136798245372689514814253769695417382'))
-
-def eliminate(values):
-    """Eliminate values from peers of each box with a single value.
-
-    Go through all the boxes, and whenever there is a box with a single value,
-    eliminate this value from the set of values of all its peers.
-
-    Args:
-        values: Sudoku in dictionary form.
-    Returns:
-        Resulting Sudoku in dictionary form after eliminating values.
-    """
-    solved_vals = [box for box in values.keys() if len(values[box]) == 1]
-
-    for box in solved_vals:
-        digit = values[box]
-        for peer in peers[box]:
-            values[peer] = values[peer].replace(digit, '')
-
-    return values
-
-# Tests:
-#display(eliminate(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..')))
-
-def only_choice(values):
-    """Finalize all values that are the only choice for a unit.
-
-    Go through all the units, and whenever there is a unit with a value
-    that only fits in one box, assign the value to this box.
-
-    Input: Sudoku in dictionary form.
-    Output: Resulting Sudoku in dictionary form after filling in only choices.
-    """
-    for unit in unitlist:
-        for digit in cols:
-            items = [box for box in unit if digit in values[box]]
-            if len(items) == 1:
-                values[items[0]] = digit
-    return values
-
-# Tests:
-#display(only_choice(eliminate(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'))))
-
-def reduce_puzzle(values):
-    stop = False
-    stalled = False
-    while not stop:
-        # Check how many boxes have a determined value
-        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
-
-        # Use the Eliminate Strategy
-        values = eliminate(values)
-
-        # Your code here: Use the Only Choice Strategy
-        values = only_choice(values)
-
-        # Check how many boxes have a determined value, to compare
-        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
-
-        stalled = len([box for box in values.keys() if len(values[box]) > 1]) > 0
-
-        # If no new values were added, stop the loop.
-        stop = solved_values_before == solved_values_after
-
-        # Sanity check, return False if there is a box with zero available values:
-        if len([box for box in values.keys() if len(values[box]) == 0]):
-            return False
-
-    if stalled:
-        print("Sudoku puzzle could not be solved")
-    else:
-        print("Sudoku puzzle solved")
-
-    return values
-
-# Tests:
-#display(reduce_puzzle(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..')))
+    # If you're stuck, see the solution.py tab!
